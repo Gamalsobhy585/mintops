af30f358d3b0b9db643ad98cf00e0ef5e886ec48
Gamalsobhy585 <eng.gamalsobhi@gmail.com>
2024-08-17 23:56:48 +0300

    feat(notifications): implement notifications for task assignments and status updates
    - Created TaskAssigned notification to alert users of new task assignments.
    - Added TaskStatusUpdated notification to inform users of task status changes.
    - Configured notifications to use mail and database channels for delivery.
    
    These notifications enhance user experience by providing timely updates on task-related activities.

---
+++ app/Notifications/TaskAssigned.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class TaskAssigned extends Notification
+{
+    use Queueable;
+
+    private $task;
+
+    public function __construct($task)
+    {
+        $this->task = $task;
+    }
+
+    public function via($notifiable)
+    {
+        return ['mail', 'database'];
+    }
+
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('You have been assigned a new task.')
+                    ->action('View Task', url('/tasks/' . $this->task->id))
+                    ->line('Thank you for using our application!');
+    }
+
+    public function toArray($notifiable)
+    {
+        return [
+            'task_id' => $this->task->id,
+            'task_title' => $this->task->title,
+        ];
+    }
+}
